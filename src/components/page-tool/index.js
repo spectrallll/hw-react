c7a473178aae91da79f2e6bae0d011c8274421cb
spectrallll <tsska_tsska@mail.ru>
2023-05-29 15:24:02 +0300

    Вынес ссылку на главную в отдельный компонент

--- src/components/page-tool/index.js
+++ src/components/page-tool/index.js
@@ -1,16 +1,14 @@
 import { cn as bem } from '@bem-react/classname';
-import {Link} from "react-router-dom";
 import BasketTool from "../basket-tool";
 import './style.css';
 import {memo} from "react";
-import {useTranslation} from "../../locales";
+import MainLink from "../link-main";
 
 function PageTool({sum, amount, onOpen}) {
   const cn = bem('PageTool')
-  const {t} = useTranslation();
   return (
     <div className={cn()}>
-      <Link className={cn('link')} to={'/'}>{t('mainLink')}</Link>
+      <MainLink />
       <BasketTool sum={sum} amount={amount} onOpen={onOpen} />
     </div>
   )
