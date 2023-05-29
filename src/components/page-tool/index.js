be3ab2bbcde9628e531ee997be032c8ea45e9fcf
spectrallll <tsska_tsska@mail.ru>
2023-05-29 16:13:45 +0300

    rename folder

--- src/components/page-tool/index.js
+++ src/components/page-tool/index.js
@@ -2,7 +2,7 @@ import { cn as bem } from '@bem-react/classname';
 import BasketTool from "../basket-tool";
 import './style.css';
 import {memo} from "react";
-import MainLink from "../link-main";
+import MainLink from "../main-link";
 
 function PageTool({sum, amount, onOpen}) {
   const cn = bem('PageTool')
